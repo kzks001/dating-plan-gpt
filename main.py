187992772f2d335713148b0225797e79bb514f05
kzks001 <samuelkohzk@gmail.com>
2023-07-31 22:44:44 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,61 +1,83 @@
+"""Python file to serve as the frontend"""
 import os
+from dotenv import load_dotenv
 import streamlit as st
-from langchain.agents import create_csv_agent
-from langchain.chat_models import ChatOpenAI
 from loguru import logger
+import pinecone
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+
+dotenv_path = "secrets.env"
+load_dotenv(dotenv_path)
+
+INDEX_NAME = "dating-plan-gpt-index"
+
+st.set_page_config(page_title="DatingPlanGPT", page_icon=":robot:")
 
 
-def main() -> None:
-    """
-    Main function to run the Dating-GPT application.
+@st.cache_data
+def init_pinecone():
+    """Initialize Pinecone client"""
+    logger.info("initializing pinecone...")
+    pinecone.init(
+        api_key=os.environ.get("PINECONE_API_KEY"),  # find at app.pinecone.io
+        environment=os.environ.get("PINECONE_API_ENV"),  # next to api key in console
+    )
+    logger.info("Pinecone initialized.")
 
-    This function loads environment variables, sets up the Streamlit page,
-    creates an agent using the OpenAI model and a CSV file, and generates
-    a response based on the user's input question.
-    """
-    # Set up Streamlit page
-    st.set_page_config(page_title="Dating-GPT")
-    st.header("Generate a dating plan with Dating-GPT")
 
-    # Get user input
-    user_question: str = st.text_area("Ask for a dating plan.")
+def load_chain() -> RetrievalQA:
+    """Logic for loading the chain you want to use should go here."""
+    llm = ChatOpenAI(temperature=0.5, model="gpt-4")
+    embeddings = OpenAIEmbeddings()
+    vectorstore = Pinecone.from_existing_index(index_name=INDEX_NAME, embedding=embeddings)
 
-    # Create an agent using the OpenAI model and a CSV file
-    api_key = os.getenv("OPENAI_API_KEY")  # Get the API key from environment variable
-    llm = ChatOpenAI(openai_api_key=api_key, temperature=0.5, model="gpt-4")
+    prompt_template = """Act as a date planner and generate a creative and flexible dating plan based on the context and requirements provided. 
+    Requirements: Your plan should be adaptable to various user constraints such as time, preferences, and other specific requirements. 
+    Ensure your responses are diverse and not limited to a fixed number of locations or repetitive suggestions. 
+    Use your creativity to provide unique and engaging dating plans for each query. 
+    Make sure the timing of the activities in the plan makes sense; for longer available hours, suggest more than one item in the plan. 
+    As you currently do not have the ability to remember past conversations, ensure each response is tailored to the specific query at hand. 
+    Do not include "overall rating" and "category" when giving the user your final answer. 
+    Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    {context}
+    When coming up with locations to suggest, you must use name field, which is the name of the restaruant/location/mall/museum. Do not use imaginary or made-up names.
+    Also, Take into account of the caption, category and opening_hours field.
+    Format the response in time-table like manner, and explain why you chose the locations you chose. Make them engaging and enticing.
+    Question: {question}
 
-    csv_file: str = "data/main.csv"
-    agent = create_csv_agent(llm, csv_file, verbose=True)
+    Answer: """
 
-    # Generate a response if the user input is not empty
-    if user_question is not None and user_question != "":
-        prompt: str = f"""Act as a date planner and generate a creative and flexible dating plan based on the context and requirements provided. 
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 
-        Context: You are given a location's summary of potential dating activities derived from Google reviews. 
+    chain_type_kwargs = {"prompt": PROMPT}
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vectorstore.as_retriever(search_kwargs={"k": 10}),
+        chain_type_kwargs=chain_type_kwargs,
+        return_source_documents=True,
+    )
 
-        Requirements: Your plan should be adaptable to various user constraints such as time, preferences, and other specific requirements. 
-        Ensure your responses are diverse and not limited to a fixed number of locations or repetitive suggestions. 
-        Use your creativity to provide unique and engaging dating plans for each query. 
-        Make sure the timing of the activities in the plan makes sense; for longer available hours, suggest more than one item in the plan. 
-        As you currently do not have the ability to remember past conversations, ensure each response is tailored to the specific query at hand. 
-        Do not include "overall rating" and "category" when giving the user your final answer. 
-
-        Question: {user_question}
-
-        Answer: """
-
-        # Log the prompt
-        logger.info(f"Prompt: {prompt}")
-
-        # Run the agent and get the response
-        response = agent.run(prompt)
-
-        # Log the response
-        logger.info(f"Response: {response}")
-
-        # Write the response to the Streamlit page
-        st.write(response)
+    return qa
 
 
 if __name__ == "__main__":
-    main()
+    init_pinecone()  # Initialize Pinecone client
+    chain = load_chain()
+
+    # From here down is all the StreamLit UI.
+    st.header("Generate a dating plan with Dating-GPT")
+
+    # Get user input
+    user_input: str = st.text_area(
+        "Ask for a dating plan.",
+        placeholder="For e.g. I am available from 5pm to 9pm. Help me plan a date to a museum and japanese restaurant",
+    )
+
+    if user_input:
+        output = chain({"query": user_input})
+        st.write(output["result"])
