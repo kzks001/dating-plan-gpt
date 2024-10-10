c096436c5c4766cc32d3bf8453527c1ee25d9365
Samuel Koh Zhi Kang <samuelkohzk@gmail.com>
2024-10-10 09:38:17 +0800

    fix: update script due to change in pinecone api

--- setup_index.py
+++ setup_index.py
@@ -1,9 +1,9 @@
 import os
-import pinecone
+from pinecone import Pinecone, ServerlessSpec
 from dotenv import load_dotenv
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Pinecone
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
 from loguru import logger
 
 dotenv_path = "./secrets.env"
@@ -13,27 +13,36 @@ INDEX_NAME = "dating-plan-gpt-index"
 DIMENSION = 1536
 
 
-def create_pinecone_index_if_not_exists(index_name: str, dimension: int = 1536):
+def create_pinecone_index_if_not_exists(
+    pinecone, index_name: str, dimension: int = 1536
+):
     """Create a pinecone index if it does not exist.
 
     Args:
         index_name (str): The name of the index to create.
     """
+    existing_indexes = [index_info["name"] for index_info in pinecone.list_indexes()]
     logger.info("initializing pinecone...")
-    if index_name in pinecone.list_indexes():
-        logger.info(f"Index {index_name} already exists. Skipping index creation.")
-        return
+    if INDEX_NAME not in existing_indexes:
+        pinecone.create_index(
+            name=index_name,
+            dimension=dimension,
+            metric="cosine",
+            spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+        )
+        logger.info(f"Creating Index {index_name}.")
+    logger.info(f"Index {index_name} already exists. Skipping index creation.")
 
-    logger.info(f"Creating index {index_name}")
-    pinecone.create_index(name=index_name, dimension=dimension)
+    return pc.Index(index_name)
 
 
 if __name__ == "__main__":
-    pinecone.init(
+    pc = Pinecone(
         api_key=os.environ.get("PINECONE_API_KEY"),  # find at app.pinecone.io
-        environment=os.environ.get("PINECONE_API_ENV"),  # next to api key in console
     )
-    create_pinecone_index_if_not_exists(index_name=INDEX_NAME, dimension=DIMENSION)
+    index = create_pinecone_index_if_not_exists(
+        pc, index_name=INDEX_NAME, dimension=DIMENSION
+    )
 
     # load the data
     loader = CSVLoader(file_path="./data/reviews.csv")
@@ -41,8 +50,9 @@ if __name__ == "__main__":
     embeddings = OpenAIEmbeddings()
     logger.info("Indexing documents...")
     try:
+        vector_store = PineconeVectorStore(index=index, embedding=embeddings)
         # docsearch = Pinecone.from_texts([t.page_content for t in documents], embeddings, index_name=INDEX_NAME)
-        docsearch = Pinecone.from_documents(documents, embeddings, index_name=INDEX_NAME)
+        vector_store.add_documents(documents=documents)
     except Exception as e:
         logger.error(e)
         raise e
